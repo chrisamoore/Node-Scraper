d90ec819be79a3e967114be2ddf8d7f64b2a0616
Christopher A. Moore <chris.a.moore@gmail.com>
2013-03-01 13:07:37 -0800

    Mongo in Node.io Issues

--- hello.js
+++ hello.js
@@ -1,5 +1,9 @@
 var nodeio = require('node.io');
+// Mongo DB server Connection
+var MongoClient = require('mongodb').MongoClient;
+var server = "mongodb://127.0.0.1:27017/test";
 
+// Connect to the db
 var options = {
     timeout: 10,    //Timeout after 10 seconds
     max: 20,        //Run 20 threads concurrently (when run() is async)
@@ -9,14 +13,29 @@ var options = {
 var methods = {
     input : false,
     run: function() {
+        this.insert({ str: "foobar" });
         this.emit('Pass');
         return;
     },
     fail: function (input, status) {
-        //status = "timeout"
         this.emit('Fail'); //You still need to complete the thread with an emit or skip, etc.
+    },
+    insert: function(arg){
+        this.emit(arg);
+        MongoClient.connect(server, function(err, db, arg) {
+          if(err) { return console.dir(err); }
+          console.dir('Mongo Connected');
+          console.dir(arg);
+          db.collection("test").insert(arg, function (err, inserted) {
+                if (err) {
+                    console.dir(err);
+                    return;
+                }
+                console.dir(inserted);
+                return;
+            });
+        });
     }
 };
-
 
 exports.job = new nodeio.Job(options, methods);
